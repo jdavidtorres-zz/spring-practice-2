5a1f34be8aa55262110884d0ac0b1b9c4adb1554
Jesus David Torres <jesu_ti91@hotmail.com>
2020-08-08 16:29:13 -0500

    Solo formato

--- conference/src/main/java/co/com/jdti/practice/conference/models/Session.java
+++ conference/src/main/java/co/com/jdti/practice/conference/models/Session.java
@@ -1,6 +1,13 @@
 package co.com.jdti.practice.conference.models;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity(name = "sessions")
