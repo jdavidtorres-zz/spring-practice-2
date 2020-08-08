e8fd197b78f162594ab852099edf6f591793d8fa
Jesus David Torres <jesu_ti91@hotmail.com>
2020-08-08 16:24:07 -0500

    Controllers

---
+++ conference/src/main/java/co/com/jdti/practice/conference/controllers/SessionsController.java
@@ -0,0 +1,35 @@
+package co.com.jdti.practice.conference.controllers;
+
+import co.com.jdti.practice.conference.models.Session;
+import co.com.jdti.practice.conference.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+}
