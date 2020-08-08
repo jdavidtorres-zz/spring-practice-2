7e2651d8e6066dd4610434bf4b9a749ad0d60206
Jesus David Torres <jesu_ti91@hotmail.com>
2020-08-08 16:46:32 -0500

    Operacion de actualizar

--- conference/src/main/java/co/com/jdti/practice/conference/controllers/SessionsController.java
+++ conference/src/main/java/co/com/jdti/practice/conference/controllers/SessionsController.java
@@ -2,6 +2,7 @@ package co.com.jdti.practice.conference.controllers;
 
 import co.com.jdti.practice.conference.models.Session;
 import co.com.jdti.practice.conference.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -36,6 +37,14 @@ public class SessionsController {
     @RequestMapping(value = "{id}")
     public void delete(@PathVariable Long id) {
         sessionRepository.deleteById(id);
+    }
+
+    @PutMapping
+    @RequestMapping(value = "{id}")
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 
 }
